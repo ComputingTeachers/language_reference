a826bcb1b3cce0cd0cd41fb5347a0cde6641ad02
Allan Callaghan <allan.callaghan@global.com>
2026-02-14 20:52:12 +0000

    Added workflow files for languages individually Added workflow generator for workflow files Move make_ver out of root

---
+++ language_reference/generate_github_workflows.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = "~=3.14"
+# dependencies = [
+#   "pyyaml",
+# ]
+# ///
+"""
+Generates Github Actions Workflows for languages.
+
+To generate for every language:
+    ./generate_github_workflows.py
+"""
+
+from collections.abc import Set
+from pathlib import Path
+from string import Template
+
+import yaml
+
+github_workflow_template = Template(
+    """
+name: language_reference_$LANGUAGE
+
+on:
+  push:
+    branches:
+      - main
+    paths:
+      - ".github/workflows/language_reference_$LANGUAGE.yml"
+      - "make_ver/**"
+      - "language_reference/languages/$LANGUAGE"
+
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@main
+
+      - name: $LANGUAGE
+        run: docker compose --project-directory language_reference up --build $LANGUAGE
+""".lstrip()
+)
+
+LANGUAGES: Set[str] = frozenset(
+    yaml.safe_load(Path("compose.yaml").read_bytes())["services"].keys()
+)
+
+PATH_WORKFLOWS = Path("../").joinpath(".github/workflows/")
+
+for language in LANGUAGES:
+    PATH_WORKFLOWS.joinpath(f"language_reference_{language}.yml").write_text(
+        github_workflow_template.substitute(LANGUAGE=language)
+    )
